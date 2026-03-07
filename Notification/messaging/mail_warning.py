fa133ac56700922eb488372212ec7a0aaabbf0df
TanmayJain09 <tanmayjain9117@gmail.com>
2026-03-07 11:44:14 +0530

    send_email function added

--- Notification/messaging/mail_warning.py
+++ Notification/messaging/mail_warning.py
@@ -20,3 +20,17 @@ EMAIL_PASSWORD = "nokgtfpcprbmuybe"  # Use Gmail App Password
 #smtp settings
 SMTP_SERVER = "smtp.gmail.com"
 PORT = 465  # For SSL
+
+#function to send email
+def send_email(to_email, subject, body) :
+    msg = EmailMessage()
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.set_content(body)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server : 
+        server.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+        server.send_message(msg)
+        print(f"Email sent to {to_email}")

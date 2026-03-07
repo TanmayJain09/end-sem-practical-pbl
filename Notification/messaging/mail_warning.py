3ea81204a9caca68811b47e54c596ce38b072676
TanmayJain09 <tanmayjain9117@gmail.com>
2026-03-07 11:30:40 +0530

    mail_warning.py added

---
+++ Notification/messaging/mail_warning.py
@@ -0,0 +1,14 @@
+#importing library
+import pandas as pd
+import smtplib, ssl
+from email.message import EmailMessage
+from pathlib import Path
+
+#path
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROCESSED_DIR = PROJECT_ROOT / "data" / "processed"
+CSV_FILE = PROCESSED_DIR / "critical_contacts.csv"
+
+#reading csv
+df = pd.read_csv(CSV_FILE)
+print(df)

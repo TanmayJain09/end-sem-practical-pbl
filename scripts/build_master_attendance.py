ef80fe5130a001101c4365b5c2f30f07ad33358b
TanmayJain09 <tanmayjain9117@gmail.com>
2026-03-13 17:17:34 +0530

    updating attendance pipeline added

--- scripts/build_master_attendance.py
+++ scripts/build_master_attendance.py
@@ -54,7 +54,7 @@ def load_master():
 
     return pd.DataFrame()
 
-def process_attendance(file_path) : 
+def process_attendance_file(file_path) : 
     """
     Process a raw attendance CSV and return a dataframe
     ready to append into master_attendance.csv
@@ -74,3 +74,53 @@ def process_attendance(file_path) :
     df["Source_File"] = metadata["Source_File"]
 
     return df
+
+def update_master_attendance() : 
+    
+    #load registry
+    registry = load_registry()
+
+    #get all the attendance files
+    files = get_attendance_files()
+
+    #detect changed files
+    changed_files = detect_changed_files(files,registry)
+
+    if not changed_files : 
+        print("No new or modified files")
+        return
+
+    #load master dataframe
+    master_df = load_master()
+
+    new_rows = []
+
+    for file_path, file_hash in changed_files : 
+        try : 
+            df = process_attendance_file(file_path)
+
+            new_rows.append(df)
+
+            #update registry
+            registry[file_path.name] = file_hash
+
+            print(f"Processed: {file_path.name}")
+
+        except Exception as e : 
+            print(f"Error Processing {file_path.name} : {e}")
+    
+    if new_rows : 
+        
+        combined_df = pd.concat(new_rows,ignore_index=True)
+        
+        master_df = pd.concat([master_df, combined_df], ignore_index=True)
+
+        master_df.to_csv(MASTER_FILE, index=False)
+
+        print(f"Master attendance updated: {MASTER_FILE}")
+    
+    #save registry
+    save_registry(registry)
+
+if __name__ == "__main__":
+    update_master_attendance()

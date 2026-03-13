03dc852679df5593a12ae72c7df14a775ab113d2
TanmayJain09 <tanmayjain9117@gmail.com>
2026-03-13 16:33:39 +0530

    file_parser.py for getting the code from file name added

---
+++ scripts/build_master_attendance.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import pandas as pd
+
+from scripts.hash_utils import compute_file_hash
+from scripts.registry_manager import (
+    load_registry,
+    save_registry,
+    get_file_hash
+)
+from scripts.file_parser import parse_attendance_filename
+
+RAW_ATTENDANCE_DIR = Path("data/raw_attendance")
+print(RAW_ATTENDANCE_DIR)
